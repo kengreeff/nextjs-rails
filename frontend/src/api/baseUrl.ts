fa3bcc53c5276851804ddba3562090a1d3e6a020
Ken Greeff <ken@kengreeff.com>
2024-09-05 14:57:46 +1000

    Update baseUrl.ts

--- frontend/src/api/baseUrl.ts
+++ frontend/src/api/baseUrl.ts
@@ -1,7 +1,7 @@
 const baseUrls = {
     development: 'http://localhost:3002/v1/',
     staging: '',
-    production: '',
+    production: 'https://backend-shy-dawn-8299.fly.dev/v1/',
     test: '',
 }
 
