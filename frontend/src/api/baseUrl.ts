c1faba6980b4ef8d2e41abacf5fcdc3a1ed61323
Ken Greeff <ken@kengreeff.com>
2024-09-13 14:03:23 +1000

    Deploy to vercel

--- frontend/src/api/baseUrl.ts
+++ frontend/src/api/baseUrl.ts
@@ -1,10 +1,10 @@
 const baseUrls = {
     development: 'http://localhost:3002/v1/',
     staging: '',
-    production: '',
+    production: 'https://backend-small-forest-9061.fly.dev/v1/',
     test: '',
 }
 
-const baseUrl = baseUrls[process.env.NODE_ENV || 'development']
+const baseUrl = baseUrls[process.env.NEXT_PUBLIC_APP_ENV || process.env.NODE_ENV || 'development']
 
 export default baseUrl
